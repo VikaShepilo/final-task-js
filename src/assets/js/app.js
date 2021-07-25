3da6613620cc09d5a33d0efb8210131043468cd5
VikaShepilo <shepilovika@gmail.com>
2021-07-25 14:04:37 +0300

    create modal window

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,82 +1,31 @@
-// WINDOW WIDTH DETECT
-let currentWidth = $(window).width();
-$(window).on('resize', function () {
-    currentWidth = $(window).width();
-});
-
-//App
-const Application = {
-    init: function() {
-        this.events();
-    },
-    events: function() {
-        !window.reinit && (window.reinit = {});
-
-        //main events
-        this.eventList.lazyImgs();
-        this.eventList.svgPolifill();
-        this.eventList.mask();
-
-        //app events
-        //this.eventList.sliders();
-    },
-    eventList: {
-        lazyImgs: function(){
-
-            let callback_loaded = function(el) {
-                const $img = $(el),
-                      $img_wrap = $img.closest('.lazy-img-wrap');
-                $img_wrap.addClass('loaded');
-            };
-
-            new LazyLoad({
-                elements_selector: ".lazy-img",
-                threshold: 0,
-                callback_loaded: callback_loaded,
-            });
-        },
-        svgPolifill: function () {
-            // svg sprites
-            svg4everybody && svg4everybody();
-        },
-        mask: function() {
-            $("[type=tel]").inputmask({
-                mask: '+7 (999) 99-99-99',
-                "clearIncomplete": false,
-                "showMaskOnHover": false,
-                "showMaskOnFocus": true,
-                "clearMaskOnLostFocus": true
-            });
-        },
-
-        sliders: function(){
-            $('.js-swiper-banner').each(function(i){
-                let bannerSwiper = new Swiper($('.js-swiper-banner .swiper-container')[i], {
-                    // Disable preloading of all images
-                    //preloadImages: false,
-                    // Enable lazy loading
-                    //lazy: {
-                    //    loadPrevNext: true,
-                    //},
-                    autoplay: {
-                        delay: 5000,
-                    },
-                    loop: true,
-                    spaceBetween: 15,
-
-                    // Navigation arrows
-                    navigation: {
-                        nextEl: $('.js-swiper-banner .swiper-button-next')[i],
-                        prevEl: $('.js-swiper-banner .swiper-button-prev')[i],
-                    },
-                });
-            });
-            
-        },
+function hideShow() {
+    var div = document.getElementById("prompt-form-container").style.display;
+    if(div == "none") {
+        div = "block";
+        document.body.style.overflowY = 'hidden';
+    } else {
+        div = "none";
+        document.body.style.overflowY = 'visible';
     }
-};
-$(function () {
-    'use strict';
-    Application.init();
-});
+    document.getElementById("prompt-form-container").style.display = div;
+}
 
+let form = document.querySelector('.prompt-form');
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    let name1 = document.querySelector('.prompt-email').value ;
+    let lastName = document.querySelector('.prompt-password').value ;
+    form.reset ();
+    document.querySelector('.prompt-button1').disabled = true;
+}
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    let input1 = document.querySelector('.prompt-email');
+    if (input1.value == ''){
+        input1.focus()
+    }
+    var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    return re.test(String(email));
+}
