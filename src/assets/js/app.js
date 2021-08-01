175a8c514efb81f909f2a84c5a7ad15dd9e9501f
VikaShepilo <shepilovika@gmail.com>
2021-08-01 08:06:12 +0300

    window

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,26 +1,15 @@
-function hideShow() {
-    var div = document.getElementById("prompt-form-container").style.display;
-    if(div == "none") {
-        div = "block";
-        document.body.style.overflowY = 'hidden';
-    } else {
-        div = "none";
-        document.body.style.overflowY = 'visible';
-    }
-    document.getElementById("prompt-form-container").style.display = div;
-}
 
 let form = document.querySelector('.prompt-form');
 
-form.onsubmit = (e) => {
+form.addEventListener("submit", function(e){
     e.preventDefault();
     let name1 = document.querySelector('.prompt-email').value ;
     let lastName = document.querySelector('.prompt-password').value ;
     form.reset ();
     document.querySelector('.prompt-button1').disabled = true;
-}
+})
 
-form.onsubmit = (e) => {
+form.addEventListener("submit", function(e){
     e.preventDefault();
     let input1 = document.querySelector('.prompt-email');
     if (input1.value == ''){
@@ -28,4 +17,14 @@ form.onsubmit = (e) => {
     }
     var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
     return re.test(String(email));
-}
+})
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    let input2 = document.querySelector('.prompt-password');
+    if (input2.value == ''){
+        input2.focus()
+    }
+    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    return re.test(String(password));
+})
