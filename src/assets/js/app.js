7dd7294b134ec87fa2814a31232f79ef16b5b06a
VikaShepilo <shepilovika@gmail.com>
2021-08-10 16:11:54 +0300

    ind

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,57 @@
+function initMap() {
+    var pos = {lat: 53.909129, lng: 27.470102}
+    var pos1 = {lat: 53.908344, lng: 27.484478}
+    var pos2 = {lat: 53.907308, lng: 27.492482}
 
+    var opt = {
+        center: pos1,
+        zoom: 14,
+    };
 
-var tabs = document.querySelectorAll(".tab");
-var con = document.querySelectorAll(".c");
-var temp = con[0];
-    tabs.forEach(function(item,i){
-	item.addEventListener("click", function() {
-        temp.classList.add("none");
-        temp = con[i];
-        temp.classList.remove("none")
-    })
-})
+    var map = new google.maps.Map(document.getElementById("map"), opt);
+
+    var marker = new google.maps.Marker({ 
+        map: map,
+        position: pos,
+        title: "OJJO"
+    });
+
+    var info = new google.maps.InfoWindow({ 
+        content: '<p>ТЦ Скала</p>'
+    });
+
+    marker.addListener("click", function() {
+        info.open(map, marker)
+    });
+
+    var marker1 = new google.maps.Marker({
+        map: map,
+        position: pos1,
+        title: "OJJO"
+    });
+
+    var info1 = new google.maps.InfoWindow({
+        content: '<p>ТЦ Тивали</p>'
+    });
+
+    marker1.addListener("click", function() {
+        info1.open(map, marker1)
+    });
+
+    var marker2 = new google.maps.Marker({
+        map: map,
+        position: pos2,
+        title: "OJJO"
+    });
+
+    var info2 = new google.maps.InfoWindow({
+        content: '<p>Раковский</p>'
+    });
+
+    marker2.addListener("click", function() {
+        info2.open(map, marker2)
+    });    
+}
 
 let allforms = document.querySelector('.allforms');
 let form = document.querySelector('.prompt-form');
@@ -21,34 +63,75 @@ let main = document.querySelector('.main');
 let maininfo = document.querySelector('.maininfo');
 let err = document.querySelector('.err');
 let errRegistration = document.querySelector('.errRegistration');
+let tab1 = document.querySelector('.tab-1');
+let tab2 = document.querySelector('.tab-2');
+let diventer = document.querySelector('.diventer');
+let divreg = document.querySelector('.divreg');
+let inputem = document.querySelector('.prompt-email');
+let inputpas = document.querySelector('.prompt-password');
+let inputemreg = document.querySelector('.prompt-email-registration');
+let inputpasreg = document.querySelector('.prompt-password-registration');
+let inputpas2reg = document.querySelector('.prompt-password2-registration');
+let exit = document.querySelector('.exit')
 
-var tabs = document.querySelectorAll(".tab");
-var con = document.querySelectorAll(".c");
-var temp = con[0];
-    tabs.forEach(function(item,i){
-	item.addEventListener("click", function() {
-        temp.classList.add("none");
-        temp = con[i];
-        temp.classList.remove("none")
-    })
+exit.addEventListener('click', function() {
+    document.close()
 })
 
-document.addEventListener('click', function(event) {
-    if ( event.target.className != '.allforms' ) {
-        allforms.focus();
-        //allforms.style.display = 'none'
-    };
+tab1.addEventListener("click", function(e) {
+    divreg.style.display = 'none';
+    diventer.style.display = 'block';
+})
+tab2.addEventListener("click", function(e) {
+    diventer.style.display = 'none';
+    divreg.style.display = 'block';
 })
 
-tabledisplay.addEventListener('click', function() {
-    table.style.display = 'block'
-    maininfo.style.display = 'none'
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    if (inputpas.value == ''){
+        inputpas.focus()
+    }
+    if (inputem.value == ''){
+        inputem.focus()
+        return re.test(String(inputem.value));
+    }
+    if ((localStorage.getItem("email") == inputem.value) && (localStorage.getItem("password") == inputpas.value)) {
+        allforms.style.display = "none"
+    } else {
+        err.style.display = 'block'
+        err.innerHTML = 'АККАУНТ НЕ НАЙДЕН'
+    }
 })
 
-main.addEventListener('click', function() {
-    table.style.display = 'none'
-    maininfo.style.display = 'block'
-    if (sUsrAg.indexOf("Firefox") > -1) {
+formRegistration.addEventListener("submit", function(e){
+    e.preventDefault();
+    if (inputpas2reg.value == '') {
+        inputpas2reg.focus()
+    }
+    if (inputpasreg.value == '') {
+        inputpasreg.focus() 
+    }
+    if (inputemreg.value == ''){
+        inputemreg.focus()
+        inputemreg.value == re.test(String(email))   
+    }
+    if ((inputpasreg.value != inputpas2reg.value) || (inputpasreg.value == '')) {
+        errRegistration.style.display = 'block'
+        errRegistration.innerHTML = 'Пароли не совпадают!'
+    }else {
+        inputpasreg.value = localStorage.setItem("password", inputpasreg.value)
+        inputemreg.value = localStorage.setItem("email", inputemreg.value)
+        errRegistration.style.display = 'block'
+        errRegistration.innerHTML = 'Регистрация завершена'
+        inputpas2reg.value = ''
+        inputpasreg.value = ''
+        inputemreg.value = ''
+    }
+})
+
+var sBrowser, sUsrAg = navigator.userAgent;
+if (sUsrAg.indexOf("Firefox") > -1) {
     sBrowser = "Mozilla Firefox";
      //"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"
     } else if (sUsrAg.indexOf("Opera") > -1) {
@@ -68,65 +151,18 @@ main.addEventListener('click', function() {
     } else {
         sBrowser = "unknown";
     }
-    document.getElementById('maininfo').innerHTML += sBrowser
+document.getElementById('maininfo').innerHTML = sBrowser
+
+main.addEventListener('click', function() {
+    table.style.display = 'none';
+    maininfo.style.display = 'block';
 })
 
-var sBrowser, sUsrAg = navigator.userAgent;
-
-//The order matters here, and this may report false positives for unlisted browsers.
-
-;
-
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-    let inputem = document.querySelector('.prompt-email');
-    let inputpas = document.querySelector('.prompt-password');
-    if (inputpas.value == ''){
-        inputpas.focus()
-    }
-    if (inputem.value == ''){
-        inputem.focus()
-        return re.test(String(inputem.value));
-    }
-    if ((localStorage.getItem("email") == inputem.value) && (localStorage.getItem("password") == inputpas.value)) {
-        allforms.style.display = "none"
-    } else {
-        err.style.display = 'block'
-        err.innerHTML = 'АККАУНТ НЕ НАЙДЕН'
-    }
+tabledisplay.addEventListener('click', function() {
+    maininfo.style.display = 'none';
+    table.style.display = 'block';
 })
 
-
-//нет проверки совпадение паролей!!!
-formRegistration.addEventListener("submit", function(e){
-    e.preventDefault();
-    let inputem = document.querySelector('.prompt-email-registration');
-    let inputpas = document.querySelector('.prompt-password-registration');
-    let inputpas2 = document.querySelector('.prompt-password2-registration');
-    if (inputpas2.value == '') {
-        inputpas2.focus()
-    }
-    if (inputpas.value == '') {
-        inputpas.focus() 
-    }
-    if (inputem.value == ''){
-        inputem.focus()
-        inputem.value == re.test(String(inputem.value))   
-    }
-    if (inputpas.value == inputpas2.value) {
-        inputpas.value = localStorage.setItem("password", inputpas.value)
-        inputem.value = localStorage.setItem("email", inputem.value)
-        formRegistration.style.display = "none"
-        form.style.display = "block"
-    } else {
-        errRegistration.style.display = 'block'
-        errRegistration.innerHTML = 'Пароли не совпадают!'
-    }
-    
-})
-
-
-//table
 const url = 'https://gist.githubusercontent.com/oDASCo/3f4014d24dc79e1e29b58bfa96afaa1b/raw/677516ee3bd278f7e3d805108596ca431d00b629/db.json'
 
 let arr = []
@@ -156,7 +192,6 @@ function returnMaxBalance(balance) {
     return maxBalance
 }
 
-// table
 fetch(url)
     .then(r => r.json())
     .then(r => {
@@ -183,8 +218,9 @@ fetch(url)
 
             if (arr.gender == "male") ++male
             if (arr.gender == "female") ++female
-        })
 
+        })
+        
         document.getElementById('app1').innerHTML += returnMaxAge(arrAge)
         document.getElementById('app2').innerHTML += returnMaxBalance(arrBalance)
         document.getElementById('app3').innerHTML += male
@@ -203,4 +239,4 @@ fetch(url)
 let buttonTop = document.querySelector('.button-top')
 buttonTop.addEventListener("click", () => { 
     window.scrollTo({ top: 0, behavior: 'smooth'});
-});
+})
